724dca3c4054f9c4719666b56168b02c640da7c2
Amorem <dev@amorem.com>
2021-02-06 16:39:04 +0100

    Add Search enpoint

--- server/src/routes/video.js
+++ server/src/routes/video.js
@@ -7,6 +7,7 @@ function getVideoRoutes() {
   const router = express.Router();
   router.get("/", getRecommendedVideos);
   router.get("/trending", getTrendingVideos);
+  router.get("/search", searchVideos);
 
   return router;
 }
@@ -70,7 +71,46 @@ async function getTrendingVideos(req, res) {
   });
 }
 
-async function searchVideos(req, res, next) {}
+async function searchVideos(req, res, next) {
+  if (!req.query.query) {
+    return next({
+      message: "Enter a search query",
+      statusCode: 400,
+    });
+  }
+  let videos = await prisma.video.findMany({
+    include: {
+      user: true,
+    },
+    where: {
+      OR: [
+        {
+          title: {
+            contains: req.query.query,
+            mode: "insensitive",
+          },
+        },
+        {
+          description: {
+            contains: req.query.query,
+            mode: "insensitive",
+          },
+        },
+      ],
+    },
+  });
+  if (!videos.length) {
+    return res.status(200).json({
+      videos,
+    });
+  }
+
+  videos = await getVideoViews(videos);
+
+  res.status(200).json({
+    videos,
+  });
+}
 
 async function addVideo(req, res) {}
 
