8470cf8aa98ff429436ecf221ce2717e7887af70
Amorem <dev@amorem.com>
2021-02-06 16:03:00 +0100

    videos/trendingVideos endpoint

--- server/src/routes/video.js
+++ server/src/routes/video.js
@@ -1,14 +1,74 @@
+import { PrismaClient } from "@prisma/client";
 import express from "express";
+
+const prisma = new PrismaClient();
 
 function getVideoRoutes() {
   const router = express.Router();
+  router.get("/", getRecommendedVideos);
+  router.get("/trending", getTrendingVideos);
 
   return router;
 }
 
-async function getRecommendedVideos(req, res) {}
+async function getVideoViews(videos) {
+  for (const video of videos) {
+    const views = await prisma.view.count({
+      where: {
+        videoId: {
+          equals: video.id,
+        },
+      },
+    });
+    video.views = views;
+  }
+  return videos;
+}
 
-async function getTrendingVideos(req, res) {}
+async function getRecommendedVideos(req, res) {
+  let videos = await prisma.video.findMany({
+    include: {
+      user: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  if (!videos.length) {
+    return res.status(200).json({
+      videos,
+    });
+  }
+
+  videos = await getVideoViews(videos);
+
+  res.status(200).json({
+    videos,
+  });
+}
+
+async function getTrendingVideos(req, res) {
+  let videos = await prisma.video.findMany({
+    include: {
+      user: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  if (!videos.length) {
+    return res.status(200).json({
+      videos,
+    });
+  }
+
+  videos = await getVideoViews(videos);
+  videos.sort((a, b) => b.views - a.view);
+
+  res.status(200).json({
+    videos,
+  });
+}
 
 async function searchVideos(req, res, next) {}
 
