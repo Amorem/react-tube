b96e0d5c89a107cdfefc431fba9457b8905f1816
Amorem <dev@amorem.com>
2021-02-09 17:20:19 +0100

    videoCard

--- client/src/utils/date.js
+++ client/src/utils/date.js
@@ -1,1 +1,5 @@
-export function formatCreatedAt() {}
+import formatDistanceToNowStric from "date-fns/formatDistanceToNowStrict";
+
+export function formatCreatedAt(timestamp) {
+  return formatDistanceToNowStric(new Date(timestamp), { addSuffix: true });
+}
