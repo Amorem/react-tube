c7e2c0f6c9cd0f48d7190e49d86bae309052b82e
Amorem <dev@amorem.com>
2021-02-12 11:39:24 +0100

    LikedVideos

--- client/src/pages/LikedVideos.js
+++ client/src/pages/LikedVideos.js
@@ -1,12 +1,30 @@
 import React from "react";
+import { useQuery } from "react-query";
+import ErrorMessage from "../components/ErrorMessage";
 import { ChannelIcon } from "../components/Icons";
 import SignUpCard from "../components/SignUpCard";
+import { useAuth } from "../context/auth-context";
 import Wrapper from "../styles/Trending";
+import { client } from "../utils/api-client";
+import Skeleton from "../skeletons/TrendingSkeleton";
+import { Link } from "react-router-dom";
+import TrendingCard from "../components/TrendingCard";
 
 function LikedVideos() {
-  const isAuth = false;
+  const user = useAuth();
+  const {
+    data: videos,
+    isLoading,
+    isError,
+    error,
+    isSuccess,
+  } = useQuery(
+    "LikedVideos",
+    () => client.get("/users/liked-videos").then((res) => res.data.videos),
+    { enabled: user }
+  );
 
-  if (!isAuth) {
+  if (!user) {
     return (
       <SignUpCard
         icon={<ChannelIcon />}
@@ -16,11 +34,25 @@ function LikedVideos() {
     );
   }
 
+  if (isLoading) return <Skeleton />;
+  if (isError) return <ErrorMessage error={error} />;
+
   return (
     <Wrapper>
       <h2>Liked Videos</h2>
-      <p className="secondary">Videos that you have liked will show up here</p>
-      Liked Videos
+
+      {isSuccess && !videos.length && (
+        <p className="secondary">
+          Videos that you have liked will show up here
+        </p>
+      )}
+      {isSuccess && videos.length
+        ? videos.map((video) => (
+            <Link to={`/watch/${video.id}`} key={video.id}>
+              <TrendingCard video={video} />
+            </Link>
+          ))
+        : null}
     </Wrapper>
   );
 }
